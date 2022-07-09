48bbf29364ca5ab7403cad74de980e88bcc92579
od <poseidonog@gmail.com>
2022-07-09 15:28:28 -0400

    components, props and propTypes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
+import Navbar from "./components/layouts/Navbar";
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 class App extends Component {
-      foo = () => 'bars';
+
+  
    render () {
-      const name = 'John Doe';
-      const getJob = () => 'devops';
-      const loading = false;
-      const showName = false;
-
-
+      // const nums = [1,2,5,4,7];
       return (
          <div className="App">
-            {/*Can add javascript within brackets*/}
-            {loading ? <h4>Loading..... </h4> : <h1>Hello {showName && name.toUpperCase()}</h1>}
-            {/*<h2>Job: {getJob()}</h2>*/}
-            {/*<h2>Foo: {this.foo()}</h2>*/}
+            <h1>Hello</h1>
+            <Navbar title="Github Finder" icon="fab fa-github"/>
 
          </div>
       );
